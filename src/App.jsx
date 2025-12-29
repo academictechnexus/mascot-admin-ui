ad1f985c0b54fcb8b056d715f3de765124039f58
academictechnexus <academictechnexus@gmail.com>
2025-12-29 20:13:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import Sites from "./pages/Sites";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Layout from "./components/Layout";
-
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Layout>
-                <Dashboard />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/sites"
-          element={
-            <ProtectedRoute>
-              <Layout>
-                <Sites />
-              </Layout>
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h1>Mascot Admin UI</h1>
+      <p>Minimal build test</p>
+    </div>
   );
 }
