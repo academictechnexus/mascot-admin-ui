2dbf672f8e34ba8dc6c3a888e9aa432cc7a65055
academictechnexus <academictechnexus@gmail.com>
2025-12-29 20:21:06 +0530

    Update Sites.jsx

--- src/pages/Sites.jsx
+++ src/pages/Sites.jsx
@@ -1,43 +1,10 @@
-import { useEffect, useState } from "react";
-import { adminFetch } from "../api/adminApi";
+import React from "react";
 
 export default function Sites() {
-  const [sites, setSites] = useState([]);
-
-  useEffect(() => {
-    adminFetch("/sites").then(res => {
-      if (Array.isArray(res)) {
-        setSites(res);
-      }
-    });
-  }, []);
-
   return (
     <div>
       <h1>Sites</h1>
-
-      <table border="1" cellPadding="8">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Domain</th>
-            <th>Plan</th>
-            <th>Quota</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {sites.map(site => (
-            <tr key={site.id}>
-              <td>{site.name}</td>
-              <td>{site.domain}</td>
-              <td>{site.plan}</td>
-              <td>{site.daily_quota}</td>
-              <td>{site.status}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <p>Sites page loaded successfully ✅</p>
     </div>
   );
 }
