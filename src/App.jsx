3b7a19a33dadaabb16c0f0eaea301b04c20b9db0
academictechnexus <academictechnexus@gmail.com>
2025-12-29 20:14:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
+import Login from "./pages/Login";
+
 export default function App() {
-  return (
-    <div>
-      <h1>Mascot Admin UI</h1>
-      <p>Minimal build test</p>
-    </div>
-  );
+  return <Login />;
 }
