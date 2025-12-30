16f1afefc03948c80af8d09cd974f03d4965c194
academictechnexus <academictechnexus@gmail.com>
2025-12-30 14:00:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Sites from "./pages/Sites";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
-  const token = localStorage.getItem("admin_token");
-  return token ? (
-    <ProtectedRoute>
-      <Dashboard />
-      <Sites />
-    </ProtectedRoute>
-  ) : (
-    <Login />
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Public */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Protected */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/sites"
+          element={
+            <ProtectedRoute>
+              <Sites />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Default */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
